bcfc8483ba15c4d506c15471d42182d11172ef9b
nakedsushi <louise@art19.com>
2020-05-13 16:07:02 -0700

    PLAT-425 #time 5m get rid of spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,38 +1,18 @@
 # frozen_string_literal: true
 
-require 'spork'
+require 'rspec'
 
-Spork.prefork do
-  require 'rspec'
-
-  if ENV['COVERAGE']
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter 'spec/support'
-    end
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'spec/support'
   end
-
-  RSpec.configure do |config|
-    config.mock_with :rspec
-
-    last_gc_run = Time.now
-
-    config.before do
-      GC.disable
-    end
-
-    config.after do
-      if Time.now - last_gc_run > 1.0
-        GC.enable
-        GC.start
-        last_gc_run = Time.now
-      end
-    end
-  end
-
-  require 'rspec/expectations'
-  require 'rspec/core/expecting/with_rspec'
 end
 
-Spork.each_run do
+RSpec.configure do |config|
+  config.mock_with :rspec
+
 end
+
+require 'rspec/expectations'
+require 'rspec/core/expecting/with_rspec'
