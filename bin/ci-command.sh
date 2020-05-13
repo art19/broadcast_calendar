224f1d1252d4632d97a915c73b6232db6ac31607
nakedsushi <louise@art19.com>
2020-05-13 14:18:14 -0700

    PLAT-425 #time 5m test coverage

--- bin/ci-command.sh
+++ bin/ci-command.sh
@@ -81,7 +81,7 @@ after_success() {
   echo "** after_success"
 
   if [ "x$COVERAGE" = "xtrue" ]; then
-    ~/bin/cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
+    ~/bin/cc-test-reporter format-coverage --debug -t simplecov -o "coverage/codeclimate.${TRAVIS_JOB_NUMBER}.json"
   else
     echo "** COVERAGE reporting is turned OFF"
   fi
